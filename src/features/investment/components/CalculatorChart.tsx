797eefae5bac98523053e3706b181abd4c70528a
Ray Pulido <ray_escom@outlook.com>
2026-02-22 23:20:48 -0600

    fix(calculator): set expanded chart modal width to 80vw

--- src/features/investment/components/CalculatorChart.tsx
+++ src/features/investment/components/CalculatorChart.tsx
@@ -341,7 +341,7 @@ export const CalculatorChart = ({
             </Card>
 
             <Dialog open={expanded} onOpenChange={setExpanded}>
-                <DialogContent className="!max-w-[90vw] w-[90vw] p-6">
+                <DialogContent className="!max-w-[80vw] w-[80vw] p-6">
                     <DialogHeader>
                         <DialogTitle className="text-base">{t('calculator.growthChart')}</DialogTitle>
                     </DialogHeader>
